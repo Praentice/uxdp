3614a7d20f36540a7cb0893271d6348691c5f771
Prantice <contact@dfrecipes.fr>
2022-12-07 10:29:50 +0100

    Update of the firewall template file

--- firewall/template/uxdp_template_firewall.c
+++ firewall/template/uxdp_template_firewall.c
@@ -6,6 +6,21 @@
 #include <netinet/in.h>
 #include <bpf/bpf_helpers.h>
 
+//Function to check if IP address is in network sub
+int is_ip_address_in_network(uint32_t ip, uint32_t netip, uint32_t netmask){
+  //uint32_t ip = ...; // value to check as a int value
+  //uint32_t netip = ...; // network ip to compare with as a int value
+  //uint32_t netmask = ...; // network ip subnet mask as a int value
+  if ((netip & netmask) == (ip & netmask)) {
+    return 1;
+  }
+    // is on same subnet...
+  else {
+    return 0;
+  }
+    // not on same subnet...
+}
+
 SEC("firewall")
 int myxdpprogram(struct xdp_md *ctx) {
   void *data = (void *)(long)ctx->data;
@@ -67,16 +82,59 @@ int myxdpprogram(struct xdp_md *ctx) {
 
 }
 
+
+// MODULE_PORT_DEST = Module for TCP port dest : (PROTOCOL->dest == ntohs(PORT_DEST) // COMMENT
+// MODULE_PORT_SRC = Module for TCP one port src : (PROTOCOL->src == ntohs(PORT_DEST) // COMMENT
+// MODULE_PORT_SRC_RANGE = Module for TCP range port src : ((PROTOCOL->src >= ntohs(PORT_RANGE_MIN)) && (PROTOCOL->src <= ntohs(PORT_RANGE_MAX)) // COMMENT
+
+// MODULE_IP_SRC = Module for IP address source only (One address and several) : (iphdr->src == IP_ADDR_SRC)
+// MODULE_IP_SRC_NETWORK = Module for IP address source only (Network mask) : (is_ip_address_in_network(iphdr->src,NETIP,NETMASK))
+
+// MODULE_PORT_DEST = Module for IP address destination only (One address and several) : (iphdr->dest == IP_ADDR_DEST)
+// MODULE_PORT_DEST_NETWORK = Module for IP address destination only (Network mask) : (is_ip_address_in_network(iphdr->dest,NETIP,NETMASK))
+
 /*
-            if (tcp->dest == ntohs(80)) { // Check if it is TCP port 80
-                  return XDP_DROP;
+            if (MODULE_PORT_DEST && MODULE_PORT_SRC)) { // Check if it is TCP port PORT_DEST
+              if (MODULE_IP_SRC && MODULE_IP_DEST) {
+                  return XDP_PASS;
+              }
+            }
+*/
+
+/* For only one port dest in TCP
+            if (tcp->dest == ntohs(PORT_DEST)) { // Check if it is TCP port PORT_DEST
+                  return XDP_PASS;
+            }
+*/
+
+/* For port dest and port src  in TCP
+            if ((tcp->dest == ntohs(PORT_DEST)) && ((tcp->src >= ntohs(PORT_RANGE_MIN)) && (tcp->src <= ntohs(PORT_RANGE_MAX))) { // Check if it is TCP port 80
+                  return XDP_PASS;
+            }
+*/
+
+/* For only port dest in TCP
+            if (tcp->dest == ntohs(PORT_DEST)) { // Check if it is TCP port PORT_DEST
+                  return XDP_PASS;
             }
 */
 
 /*
-            if (udp->dest == ntohs(80)) { // Check if it is UDP port 80
-                  return XDP_DROP;
+            if (udp->dest == ntohs(80) &&) { // Check if it is UDP port 80
+                  return XDP_PASS;
             }
 */
+
+/*
+// https://stackoverflow.com/questions/31040208/standard-safe-way-to-check-if-ip-address-is-in-range-subnet
+uint32_t ip = ...; // value to check
+uint32_t netip = ...; // network ip to compare with
+uint32_t netmask = ...; // network ip subnet mask
+if ((netip & netmask) == (ip & netmask))
+    // is on same subnet...
+else
+    // not on same subnet...
+*/
+
 
 char _license[] SEC("license") = "GPL";
